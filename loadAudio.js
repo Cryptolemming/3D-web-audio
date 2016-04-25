79f7e5080640c0e6588a530430eb95b9b3f9df7a
cryptolemming <aliayoub22@gmail.com>
2016-04-24 21:07:51 -0400

    loaded mp3 and start

--- loadAudio.js
+++ loadAudio.js
@@ -1,50 +1,40 @@
 // declare required
 let audioCtx, source, analyser;
-const URL = '#';
+const URL = 'http://sampleswap.org/mp3/artist/92209/construct_Hummingbird-RMX-Edit-3-320.mp3';
 
 // determine Web Audio API support and set context
-try {
-	if(typeof webkitAudioContext === 'function' || 'webkitAudioContext' in window) {
-		audioCtx = new webkitAudioContext();
-	}
-	else {
-		audioCtx = new AudioContext();
-	}
+audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+getData = () => {
+	// create a buffer source node
+	source = audioCtx.createBufferSource();
+
+	// load audio with XMLHttpRequest for asynchronous loading
+	let request = new XMLHttpRequest();
+	request.open('GET', URL, true);
+	request.responseType = 'arraybuffer';
+	
+	request.onload = () => {
+		// and decode asynchronously after loading
+		audioCtx.decodeAudioData(request.response).then((buffer) => {
+			// if audio doesn't decode properly, inform and return
+			if (!buffer) {
+				let div = document.createElement('div');
+				div.textContent = 'Audio did not load properly';
+
+				document.body.appendChild(div);
+				return;
+			}
+
+			source.buffer = buffer;
+
+			// connect the buffer to the destination node
+			source.connect(audioCtx.destination);
+		});
+	};
+
+	request.send();
 }
-catch(e) {
-	let div = document.createElement('div');
-	div.textContent = 'Web Audio API is not supported in this browser';
 
-	document.body.appendChild(div);
-}
-
-// load audio with XMLHttpRequest for asynchronous loading
-let request = XMLHttpRequest;
-request.open('GET', URL, true);
-request.responseType = 'arraybuffer';
-
-// and decode asynchronously after loading
-request.onload = () => {
-	audioCtx.decodeAudioData(request.response).then((decodedData) => {
-		// if audio doesn't decode properly, inform and return
-		if (!decodedData) {
-			let div = document.createElement('div');
-			div.textContent = 'Audio did not load properly';
-
-			document.body.appendChild(div);
-			return;
-		}
-
-		// set the source buffer to the decoded data
-		source.buffer = decodedData;
-
-		// connect the buffer to the destination node
-		source.connect(audioCtx.destination);
-
-	});
-};
-
-
-request.send();
-
+getData();
 source.start(0);
